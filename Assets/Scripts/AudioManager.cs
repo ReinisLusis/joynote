78a4d50e3697d221d5cfa01dce1560b3b77bd511
Yanis Lukes <pendrokar@gmail.com>
2020-02-01 14:24:15 +0200

    playback source audio managers; CSV Reader

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public AudioSource goodTrack;
+	public AudioSource badTrack;
+	//public float badTrackOffset;
+	public float balance;
+	
+	// Start is called before the first frame update
+	void Start()
+    {
+
+	}
+
+	void OnGUI()
+	{
+		//balance = EditorGUILayout.Slider(balance, 0, 1);
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		goodTrack.volume = balance;
+		badTrack.volume = 1 - balance;
+	}
+}
---
+++ Assets/Scripts/MIDIToCSVReader.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class MIDIToCSVReader : MonoBehaviour
+{
+	public TextAsset textAsset;
+	private ArrayList timeOccupied = new ArrayList(); // occupied event times
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		string fs = textAsset.text;
+		string[] fLines = Regex.Split(fs, "\n|\r|\r\n");
+
+		for (int i = 0; i < fLines.Length; i++)
+		{
+
+			string valueLine = fLines[i];
+			string[] values = Regex.Split(valueLine, ",");
+
+			if (values.Length <= 5)
+			{
+				continue;
+			}
+
+			int timeVal = 0;
+			int.TryParse(values[1], out timeVal);
+
+			string eventVal = values[2].ToString().Trim();
+
+			int typeVal = 0;
+			int.TryParse(values[3], out typeVal);
+
+			int positionVal = 0;
+			int.TryParse(values[4], out positionVal);
+
+			int velocityVal = 0;
+			int.TryParse(values[5], out velocityVal);
+
+			if (
+				// Check if event time is already occupied
+				!timeOccupied.Contains(timeVal)
+
+				// Only note_on_c; FIXME: returns
+				&& eventVal == "Note_on_c"
+
+			// velocity above 0 means it has a volume
+			//&& velocityVal > 30
+			)
+			{
+				timeOccupied.Add(timeVal);
+
+				//TODO: NoteBlocks
+
+				if (velocityVal < 47)
+				{
+					//TODO: Good blocks
+					//var noteBlock = new NoteBlock()
+					//{
+					//	Time = timeVal, // event time
+					//	Position = typeVal, // Channel
+					//	Type = positionVal % 4 == 0 ? 0 : 1 // Note tone
+					//};
+				}
+				else
+				{
+					//TODO: Bad blocks
+				}
+			}
+		}
+	}
+}
