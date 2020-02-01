a5300629e6087740f570fdab666fb4592e929ed6
Gvido <ps33@inbox.lv>
2020-02-01 10:56:23 +0200

    ggj

---
+++ GGJ2020/Assets/move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    
+    public float movespeed = 10;
+    public float forwardspeed;
+    public float smooth = 5.0f;
+    public float moveaway = 1.5f;
+
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * forwardspeed;
+
+        transform.position = new Vector3(Input.GetAxis("Horizontal") * moveaway , -Input.GetAxis("Vertical") * moveaway, transform.position.z);
+
+        //transform.Translate(movespeed * Input.GetAxis("Horizontal") * Time.deltaTime, -movespeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+
+
+        float tiltAroundZ = -Input.GetAxis("Horizontal") * moveaway;
+        float tiltAroundX = Input.GetAxis("Vertical") * moveaway;
+
+
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+}
+
+
+
