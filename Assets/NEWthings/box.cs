700f51402403cd3cb86f9715b01096244f8efd10
Gvidrinskis <51948874+Gvidrinskis@users.noreply.github.com>
2020-02-02 10:49:55 +0200

    Player, particles,box effects

---
+++ Assets/NEWthings/box.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour
+{
+    public GameObject FireworksAll;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("enemy"))
+        {
+           
+            Destroy(collision.gameObject);
+            GameObject firework = Instantiate(FireworksAll, transform.position, Quaternion.identity);
+            firework.GetComponent<ParticleSystem>().Play();
+            
+        }
+    }
+        
+}
